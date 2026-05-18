b8c541fe27a24158faa0d7fb4c047e2f5c6b829e
DarioM1992 <dario21992@gmail.com>
2026-05-18 16:12:32 +0200

    abilitato controllo parola nel prompt

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
 //prompt per chiedere all'utente una parola
-// const word = prompt("scrivi una sola parola");
+const word = prompt("scrivi una sola parola");
 
 
 //verifica del prompt che sia una sola parola e non numeri o appunto più parole
-// if (word === "") {
-//     alert ("ERRORE: Non hai inserito nulla!");
-// } else if (word.includes(" ")) {
-//     alert ("ERRORE: Hai inserito più di una parola");
-// } else if (!isNaN(word)) {
-//     alert ("ERRORE: i numeri non sono validi!")
-// } else {
-//     console.log(word);
-// }
+if (word === "") {
+    alert ("ERRORE: Non hai inserito nulla!");
+} else if (word.includes(" ")) {
+    alert ("ERRORE: Hai inserito più di una parola");
+} else if (!isNaN(word)) {
+    alert ("ERRORE: i numeri non sono validi!")
+} else {
+    console.log(word);
+}
 
 
 //creare una funzione che mi dica se la parola inserita è palindroma o meno
-const word = prompt("scrivi una sola parola");
+// const word = prompt("scrivi una sola parola");
 function checkWord() {
     let palindromicWord = "";
 
