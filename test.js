852929c692f329bd04e788713d0525b13afaa380
DarioM1992 <dario21992@gmail.com>
2026-05-18 16:38:14 +0200

    aggiunto nel prompt g1

--- test.js
+++ test.js
@@ -5,16 +5,16 @@
 //Dire chi ha vinto dei due utenti
 
 function pariODispari() {
-    let choiceTypeG1 = prompt("Scegli: pari o dispari!")
+    let choiceTypeG1 = prompt("Giocatore 1 scegli: pari o dispari!")
     if (choiceTypeG1 !== "pari" && choiceTypeG1 !== "dispari"){
     alert ("ERRORE: devi per forza scegliere tra pari o dispari");
     } else {
     alert ("OK!Hai scelto " + choiceTypeG1 );
     }
+    //ora dovrei aggiungere che se la scelta di G1 è pari automaticamente la scelta del G2 + dispari 
     let choiceTypeG2 = (choiceTypeG1 === "pari")? "dispari" : "pari";
      alert ("Giocatore 2 di conseguenza tu avrai " + choiceTypeG2);
 } 
 
 pariODispari();
 
-//ora dovrei aggiungere che se la scelta di G1 è pari automaticamente la scelta del G2 + dispari 
