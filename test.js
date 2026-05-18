118e96c1ad9004f717b95e63182cad70bbf050cb
DarioM1992 <dario21992@gmail.com>
2026-05-18 16:18:50 +0200

    aggiunto test.js per secondo esercizio

---
+++ test.js
@@ -0,0 +1,6 @@
+//ESERCIZIO "2"
+//Due giocatori,ognuno sceglie se pari o dispari! Quindi creare una funzione per questo
+//Inserire un mathfloor e math random per lanciare numero da 1 a 5 usando una funzione per entrambi i giocatori
+//Sommare le due cifre randomiche e capire se il risultato è appunto pari o dispari
+//Dire chi ha vinto dei due utenti
+
