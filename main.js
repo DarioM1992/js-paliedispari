ae913f77e91cf460f300c8c6176b552e50efb4d7
DarioM1992 <dario21992@gmail.com>
2026-05-18 15:54:56 +0200

    stilato in italiano le fasi che devo eseguire per il conseguimento dell'eserc.

--- main.js
+++ main.js
@@ -13,3 +13,15 @@ if (word === "") {
 } else {
     console.log(word);
 }
+
+
+//creare una funzione che mi dica se la parola inserita è palindroma o meno
+
+
+
+
+//ESERCIZIO "2"
+//Due giocatori,ognuno sceglie se pari o dispari! Quindi creare una funzione per questo
+//Inserire un mathfloor e math random per lanciare numero da 1 a 5 usando una funzione per entrambi i giocatori
+//Sommare le due cifre randomiche e capire se il risultato è appunto pari o dispari
+//Dire chi ha vinto dei due utenti
